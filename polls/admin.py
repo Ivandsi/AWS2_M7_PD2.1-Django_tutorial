fe48d72282ed873e97e40e3d130fed5b2869ed95
Ivan D.S. <ivandiazsanchez1996@gmail.com>
2025-01-31 17:00:40 +0100

    Template cambiado

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 
 from .models import Question, Choice
+    
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+    
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    list_display = ["question_text", "pub_date", "was_published_recently", "number_of_choices"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+    inlines = [ChoiceInline]
+    def number_of_choices(self, obj):
+        return obj.choice_set.count()
 
-admin.site.register(Question)
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
